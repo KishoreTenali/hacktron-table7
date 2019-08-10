b66a0514555f343af8f03119411690345e9b1e56
Mohit <mohit190392@gmail.com>
2019-08-10 11:11:57 +0530

    Project Created

---
+++ MessageQueueT7/src/main/java/com/dbs/mqueue/MessageQueueT7Application.java
@@ -0,0 +1,13 @@
+package com.dbs.mqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageQueueT7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageQueueT7Application.class, args);
+	}
+
+}
