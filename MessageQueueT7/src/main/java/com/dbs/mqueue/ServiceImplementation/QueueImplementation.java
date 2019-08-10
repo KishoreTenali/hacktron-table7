b48c6061c351e6d0a2c38ee5ce927179afcbfdad
Abinay-Bompally <bompally.abhinay@gmail.com>
2019-08-10 12:31:31 +0530

    i

---
+++ MessageQueueT7/src/main/java/com/dbs/mqueue/ServiceImplementation/QueueImplementation.java
@@ -0,0 +1,15 @@
+package com.dbs.mqueue.ServiceImplementation;
+
+import java.util.List;
+
+
+import com.dbs.mqueue.service.QueueService;
+
+public class QueueImplementation implements QueueService {
+
+	@Override
+	public List<Queue> fetchQueue() {
+		return null;
+	}
+
+}
